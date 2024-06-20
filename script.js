5e9bfe0dc195d9eae6084edf14ea6ac4f911d859
Bon-Image <bonimage@pm.me>
2024-06-20 18:17:33 +0200

    Add HTML boilerplate and add instructions to the script.js file.
    Instructions  are about how to design functions and objects, follow these rules in the entire project.

---
+++ script.js
@@ -0,0 +1,34 @@
+//-------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------- Design Guidelines ---------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+
+
+//--------------------------------------------------- functions -----------------------------------------------------
+
+
+// 1st line: Purpose
+// 2nd line: Signature (Type input1 Type input2 -> Type output)
+// 3rd line: Functional examples
+// 4th line: Extra explanations (optional)
+
+
+//--------------------------------------------------- Objects ------------------------------------------------------
+
+
+// 1st line: Purpose
+// Briefly describe the purpose of the object and what it represents or models.
+
+// 2nd line: Properties and Methods
+// List the properties (attributes) and methods (functions) of the object with their types and purposes.
+
+// 3rd line: Example Usage
+// Provide examples of how to create an instance of the object and use its properties and methods.
+
+// 4th line: Extra Explanations (optional)
+// Any additional explanations or notes about the object. 
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+
+
