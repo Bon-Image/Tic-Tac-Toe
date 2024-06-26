b109336cdf8448c87d51b24c75a46448f8475b34
Bon-Image <bonimage@pm.me>
2024-06-26 10:31:47 +0200

    Set up the game logic
    Add the skeleton for the game, which works only in the console

--- script.js
+++ script.js
@@ -33,11 +33,11 @@
 //-------------------------------------------------------------------------------------------------------------------
 
 
-//--------------------------------------------------- Gameboard Object ------------------------------------------------------
+//--------------------------------------------------- Gameboard Object ----------------------------------------------
 
 
 // Purpose: Store the gameboard as an array
-// Properties and Methods: array gameboard !!!
+// Properties and Methods: gameboard, getCell, setCell, resetBoard
 // Example: const Gameboard = Wrapped inside IIFE, Gameboard.gameboard 
 
 
@@ -45,7 +45,7 @@
 
 
 // Purpose: Create Players for the game
-// Properties and Methods: name, increaseScore, getScore, numberOfWins, increaseNumberOfWins, getNumberOfWins 
+// Properties and Methods: name,getMark, setMark ,increaseScore, getScore
 // Example: const playerOne = createPlayer("Alex"),playerOne.name, playerOne.increaseScore(), etc. 
 
 
@@ -53,7 +53,7 @@
 
 
 // Purose: Controls the flow of the game, who is the current player and how many rounds has been played, the score board
-// Properties and Methods: getRound, increaseRound, getCurrentPlayer, setCurrentPlayer
+// Properties and Methods: getRound, getPlayes, setPlayers ,increaseRound, getCurrentPlayer, setCurrentPlayer
 // Examples: const Game = Wrapped inside IIFE, Game.currentPlayer() etc. 
 
 //-------------------------------------------------------------------------------------------------------------------
@@ -68,12 +68,29 @@
 
 const Gameboard = (function () {
 
-    const gameboard = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
+    let gameboard = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
     ];
-    return { gameboard };
+    const getCell = (cellID) => gameboard[cellID[0]][cellID[1]];
+    const setCell = (cellID, mark) => {
+
+        gameboard[cellID[0]][cellID[1]] = mark;
+
+    }
+
+    let resetBoard = () => {
+
+        for (let i = 0; i < gameboard.length; i++) {
+            for (let j = 0; j < gameboard[i].length; j++) {
+                gameboard[i][j] = null;
+            }
+        }
+
+    };
+
+    return { gameboard, getCell, setCell, resetBoard };
 
 })();
 
@@ -84,19 +101,20 @@ const Gameboard = (function () {
 
 function createPlayer(name) {
 
-    // Current score of the player in the ongoing round 
+    // Each player has a mark, X or O
+    let mark = "";
+    const getMark = () => mark;
+    const setMark = (m) => mark = m;
+
+    // Current score of the player in the ongoing competition
     let score = 0;
     const increaseScore = () => score++;
     const getScore = () => score;
 
-    // Total number of wins up to the current round
-    let numberOfWins = 0; 
-    const increaseNumberOfWins = () => numberOfWins++; 
-    const getNumberOfWins = () => numberOfWins; 
-    
-    return { name, increaseScore, getScore, numberOfWins, increaseNumberOfWins, getNumberOfWins};
 
-}
+    return { name, getMark, setMark, increaseScore, getScore };
+
+};
 
 
 // purpose: Create Game 
@@ -105,23 +123,202 @@ function createPlayer(name) {
 
 const Game = (function () {
 
-    let round = 1;
-    const getRound = () => round; 
-    const increaseRound = () => round++; 
+    let players = [];
+    const getPlayers = () => players;
+    const setPlayers = (playerList) => {
 
-    let currentPlayer; 
-    const getCurrentPlayer = () => currentPlayer; 
-    const setCurrentPlayer = (player) => {
-
-        currentPlayer = player; 
+        players = playerList;
 
     }
-    return {getRound, increaseRound, getCurrentPlayer, setCurrentPlayer}; 
+
+
+    let round = 1;
+    const getRound = () => round;
+    const increaseRound = () => round++;
+
+    let currentPlayer;
+    const getCurrentPlayer = () => currentPlayer;
+    const setCurrentPlayer = (player) => {
+
+        currentPlayer = player;
+
+    }
+    return { getRound, getPlayers, setPlayers, increaseRound, getCurrentPlayer, setCurrentPlayer };
 })()
 
 //-------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------- Game Flow------------------------------------------------------
+//--------------------------------------------------- Game Flow ------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
 
 
+// Purpose: Initialize the game
+// Signature: [player1, player2] -> void
+// initializeGame([player1, player2])
 
+function initializeGame(players) {
+
+    Game.setPlayers(players);
+    Game.setCurrentPlayer(players[0]);
+    setPlayerMark(players);
+
+}
+
+
+// Purpose: Set player's mark
+// Signature: [player1, player2] -> void
+// setPlayerMark([player1, player2])
+
+function setPlayerMark(players) {
+
+    players[0].setMark("X");
+    players[1].setMark("O");
+
+}
+
+
+// Purpose: Records the player's choice. 
+// Signature: Integer -> !!!
+// play
+
+function play(cellID) {
+
+
+    if (cellNotOccupied(cellID)) {
+
+        Gameboard.setCell(cellID, Game.getCurrentPlayer().getMark());
+
+        console.log(Gameboard.gameboard);
+
+        if (hasPlayerWon(Game.getCurrentPlayer().getMark())) {
+
+            console.log(`${Game.getCurrentPlayer().name} has won the game!`);
+            updateGameStatus(Game.getCurrentPlayer());
+
+        } else {
+
+            if (gameboardIsFull()) {
+
+                console.log("This is a tie!");
+
+            };
+
+        };
+
+        switchPlayer();
+
+    };
+
+
+
+};
+
+// Purpose: Check if the selected cell is occupid
+// Signature: Integer -> Boolean 
+// Examples: cellNotOccupied(2) -> True / False
+
+function cellNotOccupied(cellID) {
+
+    return (Gameboard.getCell(cellID) === null);
+
+}
+
+
+// Purpose: Check if the board is full
+// Sgnature: void -> Boolean 
+// Exampels: boardIsNotFull() = true 
+
+function gameboardIsFull() {
+
+    return Gameboard.gameboard.every(row => {
+
+        return row.every(cell => {
+
+            return cell !== null;
+
+        });
+
+    });
+
+
+};
+
+
+// Purpose: Switchs the player after each move. 
+// Signature: void -> void 
+// Exampel: swithcPlayer()
+
+function switchPlayer() {
+
+    let [player1, player2] = Game.getPlayers();
+
+
+    if (Game.getCurrentPlayer() === player1) {
+
+        Game.setCurrentPlayer(player2);
+
+    } else {
+
+        Game.setCurrentPlayer(player1);
+
+    }
+
+}
+
+
+// Purpose: Check for the winning combination 
+// Signature: Array -> Boolean 
+// Examples: hasPlayerWon([["X", "X", "X"], ["x", "O", "x"], ["O", "O", "X"]]) -> True
+
+function hasPlayerWon(mark) {
+    const gameboard = Gameboard.gameboard;
+
+    const winningCombinations = [
+        // Rows
+        [[0, 0], [0, 1], [0, 2]],
+        [[1, 0], [1, 1], [1, 2]],
+        [[2, 0], [2, 1], [2, 2]],
+        // Columns
+        [[0, 0], [1, 0], [2, 0]],
+        [[0, 1], [1, 1], [2, 1]],
+        [[0, 2], [1, 2], [2, 2]],
+        // Diagonals
+        [[0, 0], [1, 1], [2, 2]],
+        [[0, 2], [1, 1], [2, 0]]
+    ];
+
+    return winningCombinations.some(combination =>
+        combination.every(cell => gameboard[cell[0]][cell[1]] === mark)
+    );
+
+};
+
+
+// Purpose: Updates the scores of the winning player and the winner of each three rounds in a row
+// Signature: Player -> void 
+// Examples: !!!
+
+function updateGameStatus(player) {
+
+    player.increaseScore();
+
+    if (player.getScore() === 3) {
+
+        console.log(`${Game.getCurrentPlayer().name} has won three times in a row!`);
+
+    } else {
+
+        console.log("Reset");
+        Gameboard.resetBoard();
+
+    };
+
+};
+
+
+// Purpose: Check for the gameover
+// Signature: void -> !!!
+// Examples: !!!
+
+function gameOver() {
+
+}
